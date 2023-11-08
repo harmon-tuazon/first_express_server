e9b090aefd12c9a2a0405786316d62fc6aa5137d
harmon-tuazon <harmontuazon@gmail.com>
2023-11-08 14:34:51 +0800

    finished routing. to start w/ db

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
