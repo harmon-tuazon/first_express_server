a9a3bedfb20fecb0a45af93c3bc0642c3f7a296e
harmon-tuazon <harmontuazon@gmail.com>
2023-11-10 14:09:33 +0800

    finished with DB instance and sending

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./models/users')
 
 const app = express();
+
+const dbURI = "mongodb+srv://testUser1:zTm{2023}@nodetutorial.acvyz82.mongodb.net/nodeTutorial?retryWrites=true&w=majority"
+
+mongoose.connect(dbURI)
+    .then((result) => { app.listen(3000, () => {console.log('listening to server & connected to db')})})
+    .catch((err) => {console.error(err)})
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
+
+app.get('/users', (req, res) => {
+    const newUser = new User ({
+        name: "Harmon Tuazon",
+        email: "iamthedevt@fakemail.com",
+        password: "test12341234"
+    })
+
+    newUser.save()
+        .then((result) => {res.send(result)})
+        .catch((err) => {console.error(err)})
+})
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
@@ -18,4 +38,3 @@ app.use((req, res) => {
     res.sendFile(__dirname + '/views/404.html')
 })
 
-app.listen(3000, ()=>{console.log('listening to server')})
